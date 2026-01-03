0edc34f3f53194e951b0ba217f28aad0bf113c9f
grupodvbr <grupodvbr@gmail.com>
2026-01-03 13:49:39 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,14 @@
-const CACHE_NAME = "comandas-pwa-v2";
+const CACHE_NAME = "comandas-pwa-v4";
 
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll([
-        "/",
-        "/index.html",
-        "/manifest.json",
-        "/icons/icon-192.png",
-        "/icons/icon-512.png"
+        "/CONTAGEM-DE-COMANDAS-MERCATTO/",
+        "/CONTAGEM-DE-COMANDAS-MERCATTO/index.html",
+        "/CONTAGEM-DE-COMANDAS-MERCATTO/manifest.json",
+        "/CONTAGEM-DE-COMANDAS-MERCATTO/icons/icon-192.png",
+        "/CONTAGEM-DE-COMANDAS-MERCATTO/icons/icon-512.png"
       ]);
     })
   );
